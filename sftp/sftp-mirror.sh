8f5034fd936703f5bf08490fe66211598cddb868
Edgar Pino <me@edgarpino.com>
2017-03-24 14:46:14 -0500

    initial commit

---
+++ sftp/sftp-mirror.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# THIS IS A TEMPLATE FILE
+
+# Author: Edgar Pino <epino@envano.com>
+
+# The lftp(http://lftp.tech/lftp-man.html) cli is used to mirror FTP and SFTP sites. 
+# Bash script used to sync Rackspace/Liquid web Cloud Sites. 
+# SFTP Credentials and Host
+SFTP_SERVER="ftp.host.com"
+SFTP_USER="user"
+SFTP_PWD="password!!"
+
+# Remote Directory to mirror
+SFTP_REMOTE_DIR=/
+# Local Directory to download files to
+LOCAL_DIR=/path/to/local/directory/$SFTP_USER
+
+# This script backups up everything inside the remote server into the local directory. 
+# This is a mirror sync so everything that is removed from the remote directory will be removed locally
+# To exclude other file types or directories see the project documentation (http://lftp.tech/lftp-man.html). 
+lftp sftp://$SFTP_USER:$SFTP_PWD@$SFTP_SERVER \
+	-e "mirror -c --delete --verbose=2 --exclude='.*\.(zip|tar.gz|tar|log)$' --parallel=10 --skip-noaccess $SFTP_REMOTE_DIR $LOCAL_DIR; bye"
