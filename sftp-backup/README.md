1f121ee9896b8b3048d595ee3d064ed6ad018941
Edgar Pino <me@edgarpino.com>
2017-03-30 14:19:45 -0500

    Added readme and updated script

---
+++ sftp-backup/README.md
@@ -0,0 +1,23 @@
+# How to use the sftp-mirror.sh script
+
+1. Clone the file. 
+1. Update the following variables: 
+```bash
+CLIENT_NAME='envano'
+SFTP_SERVER="ftp.host.com"
+SFTP_USER="user"
+SFTP_PWD="password!!"
+
+# Remote Directory to mirror
+SFTP_REMOTE_DIR=/ 
+
+# Local Directory to download files to
+LOCAL_DIR=/path/to/local/directory/$CLIENT_NAME/$SFTP_USER
+```
+3. Add additional files to ignore
+```bash
+--exclude='.*\.(zip|tar.gz|tar|log)$'
+```
+4. Save the file and make it executable. 
+
+
--- sftp-backup/sftp-mirror.sh
+++ sftp-backup/sftp-mirror.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
-# THIS IS A TEMPLATE FILE
+
+# Bash script used to sync sftp sites.
 
 # Author: Edgar Pino <epino@envano.com>
 
 # The lftp(http://lftp.tech/lftp-man.html) cli is used to mirror FTP and SFTP sites. 
-# Bash script used to sync Rackspace/Liquid web Cloud Sites. 
-# SFTP Credentials and Host
+
+# # # # # # # # # # # # # # # # # # # # # # #
+# SFTP Configuration and other variables    #
+# # # # # # # # # # # # # # # # # # # # # # #
+
+CLIENT_NAME='envano'
 SFTP_SERVER="ftp.host.com"
 SFTP_USER="user"
 SFTP_PWD="password!!"
 
 # Remote Directory to mirror
-SFTP_REMOTE_DIR=/
+SFTP_REMOTE_DIR=/ 
+
 # Local Directory to download files to
-LOCAL_DIR=/path/to/local/directory/$SFTP_USER
+LOCAL_DIR=/path/to/local/directory/$CLIENT_NAME/$SFTP_USER
 
 # This script backups up everything inside the remote server into the local directory. 
 # This is a mirror sync so everything that is removed from the remote directory will be removed locally
 # To exclude other file types or directories see the project documentation (http://lftp.tech/lftp-man.html). 
 lftp sftp://$SFTP_USER:$SFTP_PWD@$SFTP_SERVER \
 	-e "mirror -c --delete --verbose=2 --exclude='.*\.(zip|tar.gz|tar|log)$' --parallel=10 --skip-noaccess $SFTP_REMOTE_DIR $LOCAL_DIR; bye"
+
+# Exit, we are done
+#######################################################################
+exit 0
