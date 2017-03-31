4f151b47e8e4b2ea559e43734f787d5fb1cc372e
Edgar Pino <me@edgarpino.com>
2017-03-31 10:23:46 -0500

    Bug fixes for script

--- git/automatic-git-sync/git-sync.sh
+++ git/automatic-git-sync/git-sync.sh
@@ -9,15 +9,16 @@
 # # # # # # # # # # # # # # # # # # # # # # #
 
 # Git config
-REPO_NAME=''
-REPO_SSH_URL=''
-REPO_BRANCH=''
+REPO_NAME='somesite-com'
+REPO_SSH_URL='git@gitlab.site.com:group/somesite-com.git'
+REPO_BRANCH='master'
 REPO_BACKUP_BRANCH="$REPO_BRANCH-snapshot"
+
 # SFTP config
-SFTP_SERVER=""
-SFTP_USER=""
-SFTP_PWD=""
-SFTP_REMOTE_DIR=/path.to.site/www/
+SFTP_SERVER="sftp.somehost.com"
+SFTP_USER="username"
+SFTP_PWD="password123!"
+SFTP_REMOTE_DIR=/
 
 # Local config
 LOCAL_DIR=$(PWD)/$SFTP_SERVER
@@ -28,6 +29,25 @@ DATE=`date +%Y-%m-%d:%I:%M:%S%p`
 #  Main Script                              #                         
 # # # # # # # # # # # # # # # # # # # # # # #
 
+# Function to URL encode text
+# http://stackoverflow.com/questions/296536/how-to-urlencode-data-for-curl-command
+rawurlencode() {
+  local string="${1}"
+  local strlen=${#string}
+  local encoded=""
+  local pos c o
+
+  for (( pos=0 ; pos<strlen ; pos++ )); do
+     c=${string:$pos:1}
+     case "$c" in
+        [-_.~a-zA-Z0-9] ) o="${c}" ;;
+        * )               printf -v o '%%%02x' "'$c"
+     esac
+     encoded+="${o}"
+  done
+  echo "${encoded}"    # You can either set a return variable (FASTER) 
+  REPLY="${encoded}"   #+or echo the result (EASIER)... or both... :p
+}
 
 # Clone the repo if it doesn't exists
 if [ ! -d "$LOCAL_REPO_DIR" ]; then
@@ -48,6 +68,13 @@ else
 
 fi
 
+# Create .gitignore if it's missing
+if [ ! -d "$LOCAL_REPO_DIR/.gitignore" ]; then
+    touch $LOCAL_REPO_DIR/.gitignore
+fi
+
+
+
 # Create a snapshot of the exiting repo before making any commits and pulling files
 if [ `git branch --list $REPO_BACKUP_BRANCH` ]; then
    
@@ -59,17 +86,15 @@ fi
 
 
 # Pull Server files
-lftp sftp://$SFTP_USER:$SFTP_PWD@$SFTP_SERVER \
-	-e "mirror -c --verbose=2  --exclude-rx-from=.gitignore --exclude='.*\.(revision)$' --parallel=10 --skip-noaccess $SFTP_REMOTE_DIR $LOCAL_REPO_DIR; bye"
+lftp -e "set sftp:auto-confirm yes; open sftp://$( rawurlencode "$SFTP_USER" ):$( rawurlencode "$SFTP_PWD" )@$SFTP_SERVER; mirror -c --verbose=2  --exclude-rx-from=.gitignore --exclude='.*\.(revision)$' --parallel=10 --skip-noaccess $SFTP_REMOTE_DIR $LOCAL_REPO_DIR; bye"
 
 # Commit and push
 echo 'Committing the following changes:'
 
 git status
 git add .
-git commit -m "Automatic sync commit from server for $DATE"
+git commit -m "Automatic sync from server for $DATE"
 git push
-
 
 # Exit, we are done
 #######################################################################
